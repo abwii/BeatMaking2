a8c63c49a0d0e005eec8276adf42db4114d6f46f
Ehcrabal <ehcraball.ehcraball95@gmail.com>
2023-01-03 15:52:25 +0100

    Coucou c'est moi j'espere que tu vas bien. Je passe sous un tunnel la. Passe la bise a tout le monde de ma part. J'arrice à CDG le nouvelle aéroport le 31. Voila j'espere que t'as bien reçu mon message. Mais sinon t'as vu les manifs gilet jaunes pour l'essence ?? c'est vraiement un sale co* ce Ma*ro* !!

---
+++ js/main.js
@@ -0,0 +1,11 @@
+let popup1 = document.getElementById("popup1");
+
+function openPopup1(){
+    popup1.classList.add("open-popup");
+    console.log("1");
+}
+
+function closePopup1(){
+    popup1.classList.remove("open-popup");
+    console.log("0");
+}
